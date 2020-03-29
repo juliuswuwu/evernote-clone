1395faee5c2ccf8d6927010437aa7a2ac902ffa8
JuliusWu <aapple1994@gmail.com>
2020-03-28 21:58:19 -0700

    create notebook

--- frontend/components/home/notebook/notebook_create_form.jsx
+++ frontend/components/home/notebook/notebook_create_form.jsx
@@ -8,7 +8,9 @@ class NotebookCreateForm extends React.Component{
             title: "",
             user_id: userId
         };
+        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
     handleSubmit(e){
         e.preventDefault();
@@ -16,9 +18,42 @@ class NotebookCreateForm extends React.Component{
         this.setState({title: ""})
     }
     
+    handleChange(e) {
+        this.setState({ title: e.target.value })
+    }
+
+    closeModal() {
+        let modal = document.querySelector(".notebook-create-form-modal");
+        if (modal) {
+            modal.classList.remove("notebook-create-form-modal-active");
+            this.setState({ title: "" })
+        }
+    }
+
     render(){
         return(
             <>
+                <div className="notebook-create-form-modal">
+                    <div className="notebook-create-form">
+                        <h3>Create New Notebook</h3>
+                        <p>Notebooks are useful for grouping notes around a common topic.
+            They can be private or shared.</p>
+                        <form>
+                            <label htmlFor="notebook-title">Title</label>
+                            <br />
+                            <input type="text" autoFocus placeholder="Notebook Title" id="notebook-title" value={this.state.title}
+                                onChange={this.handleChange}
+                            />
+                            <br />
+                            <div className="notebook-create-form-buttons">
+                                <button onClick={this.closeModal}
+                                    className="cancel-notebook-create-form-button">Cancel</button>
+                                <input onClick={this.handleSubmit}
+                                    className="notebook-create-form-button" type="submit" value="Create" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
 
             </>
         )
